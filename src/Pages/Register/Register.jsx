32d9e99c89be6d1ed55ebe3a261deb9d6128b47f
ahmedsami019 <easami019192@gmail.com>
2025-12-06 22:41:49 +0600

    added content

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Register = () => {
-    return (
-        <div>
-            <h2>this is register page</h2>
-        </div>
-    );
+  return (
+    <div className="card bg-base-100 w-full mx-auto mt-20 max-w-sm shrink-0 shadow-2xl">
+      <h1 className="text-3xl font-bold text-center">Login now!</h1>
+      <div className="card-body">
+        <form className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" className="input" placeholder="Your name" />
+          <label className="label">Email</label>
+          <input type="email" className="input" placeholder="Email" />
+          <label className="label">Password</label>
+          <input type="password" className="input" placeholder="Password" />
+          <button className="btn btn-neutral mt-4">Register</button>
+        </form>
+        <p>
+          Already have an account? please{" "}
+          <Link className="text-blue-400 underline" to="/login">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvd98CXnSz26P3Cq2DAPj-htJouL3a2jM",
+  authDomain: "react-router-auth-intrigation.firebaseapp.com",
+  projectId: "react-router-auth-intrigation",
+  storageBucket: "react-router-auth-intrigation.firebasestorage.app",
+  messagingSenderId: "930611302112",
+  appId: "1:930611302112:web:4a6f309942768b52418a85"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
