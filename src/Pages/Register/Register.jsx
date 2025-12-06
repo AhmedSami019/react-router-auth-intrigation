bb3dc909f82b1bd49be177b309257cfe645dd6e6
ahmedsami019 <easami019192@gmail.com>
2025-12-06 22:56:14 +0600

    added user creation function by firebase

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,18 +1,41 @@
-import React from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import React, { useState } from "react";
 import { Link } from "react-router";
+import { auth } from "../../firebase.init";
 
 const Register = () => {
+
+    const [success, setSuccess] = useState(false)
+
+    const handleRegister = (e)=>{
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value
+        const password = e.target.password.value
+        console.log(name, email, password);
+        setSuccess(false)
+
+        // firebase works for authentication
+        createUserWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            console.log(result);
+            setSuccess(true)
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
   return (
     <div className="card bg-base-100 w-full mx-auto mt-20 max-w-sm shrink-0 shadow-2xl">
-      <h1 className="text-3xl font-bold text-center">Login now!</h1>
+      <h1 className="text-3xl font-bold text-center">Register now!</h1>
       <div className="card-body">
-        <form className="fieldset">
+        <form onSubmit={handleRegister} className="fieldset">
           <label className="label">Name</label>
-          <input type="text" className="input" placeholder="Your name" />
+          <input type="text" name="name" className="input" placeholder="Your name" />
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" name="email" className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" name="password" className="input" placeholder="Password" />
           <button className="btn btn-neutral mt-4">Register</button>
         </form>
         <p>
@@ -21,6 +44,9 @@ const Register = () => {
             Login
           </Link>
         </p>
+        {
+            success && <p className="text-green-500">user has created successfully</p>
+        }
       </div>
     </div>
   );
